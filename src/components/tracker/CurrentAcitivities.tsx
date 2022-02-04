bc3055d9bb509788f587688bbf95900fa3dd4a05
Tung Lai <laihoangtung@gmail.com>
2022-02-03 22:43:48 -0600

    add one day calendar

--- src/components/tracker/CurrentAcitivities.tsx
+++ src/components/tracker/CurrentAcitivities.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from 'react'
-import { IActivity } from 'app/interfaces'
 
 export default function CurrentActivities() {
-  return <div className="lg:w-80 border border-black">Current Activity</div>
+  return (
+    <div className="lg:w-[260px] bg-white px-4 py-4 flex flex-col">
+      <button className="bg-[#f58320] py-1 rounded-md text-white mb-4">
+        View calendar
+      </button>
+      <DayCalendar />
+    </div>
+  )
+}
+
+function DayCalendar() {
+  return <div className="bg-[#f7f7f7] rounded-lg grow"></div>
 }
