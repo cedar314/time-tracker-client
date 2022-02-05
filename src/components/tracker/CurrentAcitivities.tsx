30b2829dbc01dd93b7f7d4ac4a1ef9199ff0f9c6
Tung Lai <laihoangtung@gmail.com>
2022-02-04 18:26:12 -0600

    add one day calendar (current activity) UI

--- src/components/tracker/CurrentAcitivities.tsx
+++ src/components/tracker/CurrentAcitivities.tsx
@@ -1,16 +1,94 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { ReactComponent as ChevronSvg } from '../../assets/svg/up-chevron.svg'
 
 export default function CurrentActivities() {
   return (
-    <div className="lg:w-[260px] bg-white px-4 py-4 flex flex-col">
-      <button className="bg-[#f58320] py-1 rounded-md text-white mb-4">
-        View calendar
-      </button>
+    <div className="lg:w-[300px] bg-white px-4 py-4 flex flex-col">
+      <CalendarButton />
+      <DayControl />
       <DayCalendar />
     </div>
   )
 }
 
+function CalendarButton() {
+  return (
+    <button className="bg-[#f78f34] py-1 rounded-lg text-white">
+      View calendar
+    </button>
+  )
+}
+
+function DayControl() {
+  return (
+    <div className="flex items-center my-5">
+      <button className="h-8 rounded-lg border border-gray-300 text-[#8320F5] font-semibold text-sm text-center px-3 mr-2">
+        Today
+      </button>
+      <button className="w-8 h-8 rounded-lg border border-gray-300 mr-2">
+        <ChevronSvg fill="#8320F5" className="-rotate-90 mx-auto my-auto" />
+      </button>
+      <button className="w-8 h-8 rounded-lg border border-gray-300 mr-2">
+        <ChevronSvg fill="#8320F5" className="rotate-90 mx-auto my-auto" />
+      </button>
+      <div className="ml-auto">Thu, Feb 2</div>
+    </div>
+  )
+}
+
 function DayCalendar() {
-  return <div className="bg-[#f7f7f7] rounded-lg grow"></div>
+  const time = []
+  for (let i = 0; i < 24; i++) time.push(i)
+
+  return (
+    <div className="flex grow bg-gray-100 rounded-xl overflow-auto scrollbar-hidden relative">
+      <div>
+        {time.map((time) => (
+          <TimeCell key={time} timeIn24h={time} />
+        ))}
+      </div>
+      <div className="grow mx-2">
+        {time.map((time) => (
+          <GridCell key={time + 'cell'} />
+        ))}
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+function TimeCell({ timeIn24h }: { timeIn24h: number }) {
+  return (
+    <div className="h-14 w-[50px] relative">
+      <div className="absolute -top-[10px] right-0 text-gray-400 text-sm">
+        {timeIn24h < 12
+          ? `${timeIn24h} am`
+          : timeIn24h === 12
+          ? '12 pm'
+          : `${timeIn24h - 12} pm`}
+      </div>
+    </div>
+  )
+}
+
+function GridCell() {
+  return (
+    <div className="h-14 relative">
+      <div className="absolute top-0 rounded-full bg-gray-400 w-full h-[1.5px]"></div>
+    </div>
+  )
+}
+
+function ActivityCell({
+  name,
+  start,
+  duration,
+  isActive,
+}: {
+  name: string
+  start: number
+  duration: number
+  isActive: boolean
+}) {
+  return <div className=""></div>
 }
